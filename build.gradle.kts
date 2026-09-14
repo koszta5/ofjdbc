c476f4141f110f501633fc7af31509c0920c473c
Kosta Kostelnik <Kosta.Kostelnik+onsemi2@onsemi.com>
2026-09-14 13:00:03 +0200

    changes to enable maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,11 @@ plugins {
     kotlin("jvm") version "2.2.20-Beta2"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("com.github.ben-manes.versions") version "0.52.0"
+    `maven-publish`
 }
 
 group = "my.jdbc"
-version = "1.0-SNAPSHOT"
+version = "1.01"
 
 repositories {
     mavenCentral()
@@ -20,6 +21,17 @@ dependencies {
     implementation("org.hsqldb:hsqldb:2.7.4")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.0")
+}
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+        }
+    }
 }
 
 tasks.test {
