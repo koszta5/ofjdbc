52a7093184df8f849457e93c01001706d138a005
Kosta Kostelnik <Kosta.Kostelnik+onsemi2@onsemi.com>
2026-09-18 11:54:00 +0200

    include pom

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,10 @@ publishing {
             groupId = project.group.toString()
             artifactId = project.name
             version = project.version.toString()
+
+            pom.withXml {
+                // optional customizations
+            }
         }
     }
 }
